8153991258bce92dd2f191fe5c638c1e8d25df17
YWSIK <yuusik818@gmail.com>
2023-11-05 15:28:53 +0900

    #1 feat: websocket 설정

--- src/main/java/com/example/network/config/WebSocketConfig.java
+++ src/main/java/com/example/network/config/WebSocketConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -13,14 +15,25 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-        //엔드포인트 정의
-        registry.addHandler(signalingSocketHandler(), "/board")
+        //엔드포인트 정의, url에 웹소켓 핸들러 매핑
+        registry.addHandler(Handler(), "/board")
                 //클라이언트에서 웹소켓 서버에 요청시 모든 요청을 수용
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                //핸드셰이크 요청을 인터셉트할 인터셉터
+                .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 
     @Bean
-    public WebSocketHandler signalingSocketHandler(){
-        return new WebSocketHandler();
+    public WebSocketHandler Handler(){
+        return new Handler();
+    }
+
+    @Bean
+    //WebSocket의 메세지 버퍼 크기, 유휴시간 등, 런타임 특성을 제어하는 프로퍼티
+    public ServletServerContainerFactoryBean createWebSocketContainer(){
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
     }
 }
--- src/main/java/com/example/network/config/WebSocketHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.network.config;
-
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class WebSocketHandler extends TextWebSocketHandler {
-
-
-}
