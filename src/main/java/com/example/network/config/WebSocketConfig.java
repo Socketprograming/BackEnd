1b3dd3ef7cfb004e366c3c374eccb513956014d7
YWSIK <yuusik818@gmail.com>
2023-11-05 16:55:08 +0900

    #1 feat: 성원아 너꺼 합침

--- src/main/java/com/example/network/config/WebSocketConfig.java
+++ src/main/java/com/example/network/config/WebSocketConfig.java
@@ -13,19 +13,20 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private final WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
         //엔드포인트 정의, url에 웹소켓 핸들러 매핑
-        registry.addHandler(Handler(), "/board")
+        registry.addHandler(webSocketHandler,"/board")
                 //클라이언트에서 웹소켓 서버에 요청시 모든 요청을 수용
                 .setAllowedOrigins("*")
                 //핸드셰이크 요청을 인터셉트할 인터셉터
                 .addInterceptors(new HttpSessionHandshakeInterceptor());
-    }
-
-    @Bean
-    public WebSocketHandler Handler(){
-        return new Handler();
     }
 
     @Bean
---
+++ src/main/java/com/example/network/config/WebSocketHandler.java
@@ -0,0 +1,45 @@
+package com.example.network.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+//웹소켓 서버를 구현
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private static Set<WebSocketSession> CLIENTS = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println(session.toString());
+
+        if (CLIENTS.contains(session)) {
+            System.out.println("이미 연결된 세션입니다. > " + session);
+        } else {
+            CLIENTS.add(session);
+            System.out.println("새로운 세션입니다. > " + session);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        CLIENTS.remove(session);
+        System.out.println("세션을 닫습니다. : " + session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("입력된 메세지입니다. > " + message);
+
+        for (WebSocketSession client : CLIENTS) {
+            System.out.println("메세지를 전달합니다. > " + message);
+            client.sendMessage(message);
+        }
+    }
+}
